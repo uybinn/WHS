9b66dc0cb91a39d6dc1a6850e2fb7834389bb08b
uybinn <159413871+uybinn@users.noreply.github.com>
2024-04-04 01:57:22 +0900

    Update PCAP.c

--- PCAP.c
+++ PCAP.c
@@ -2,18 +2,17 @@
 #include <stdio.h>
 #include <pcap.h>
 #include <arpa/inet.h>
-#include "myheader.h" // 구조체 정의가 포함된 헤더 파일
+#include "myheader.h"
 
-void got_packet(u_char *args, const struct pcap_pkthdr *header,
-                const u_char *packet)
+void got_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
 {
     struct ethheader *eth = (struct ethheader *)packet;
 
-    if (ntohs(eth->ether_type) == 0x0800) { // 0x0800은 IP 타입
+    if (ntohs(eth->ether_type) == 0x0800) {
         struct ipheader *ip = (struct ipheader *)(packet + sizeof(struct ethheader));
 
-        if (ip->iph_protocol == IPPROTO_TCP) { // TCP 프로토콜인 경우
-            int ip_header_len = ip->iph_ihl * 4; // IP 헤더 길이 (바이트 단위)
+        if (ip->iph_protocol == IPPROTO_TCP) {
+            int ip_header_len = ip->iph_ihl * 4;
             struct tcpheader *tcp = (struct tcpheader *)
                                     (packet + ip_header_len + sizeof(struct ethheader));
 
@@ -34,8 +33,6 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header,
             printf("TCP 헤더\n");
             printf("  출발지 포트: %u\n", ntohs(tcp->tcp_sport));
             printf("  목적지 포트: %u\n", ntohs(tcp->tcp_dport));
-
-            // 필요한 경우 TCP 헤더에서 더 많은 정보를 추가할 수 있습니다
         }
     }
 }
@@ -43,33 +40,14 @@ void got_packet(u_char *args, const struct pcap_pkthdr *header,
 int main()
 {
     pcap_t *handle;
-    char errbuf[PCAP_ERRBUF_SIZE];
     struct bpf_program fp;
-    char filter_exp[] = "tcp"; // TCP 패킷만 캡처하기 위한 필터 표현식
-    bpf_u_int32 net;
+    char filter_exp[] = "tcp";
 
-    // 단계 1: NIC 이름이 ens33인 라이브 pcap 세션 열기
-    handle = pcap_open_live("ens33", BUFSIZ, 1, 1000, errbuf);
-    if (handle == NULL) {
-        fprintf(stderr, "장치 %s 열기 실패: %s\n", "ens33", errbuf);
-        return EXIT_FAILURE;
-    }
+    handle = pcap_open_live("ens33", BUFSIZ, 1, 1000, NULL);
+    pcap_compile(handle, &fp, filter_exp, 0, PCAP_NETMASK_UNKNOWN);
+    pcap_setfilter(handle, &fp);
+    pcap_loop(handle, -1, got_packet, NULL)
+    pcap_close(handle);
 
-    // 단계 2: filter_exp를 BPF 의사 코드로 컴파일
-    if (pcap_compile(handle, &fp, filter_exp, 0, net) == -1) {
-        fprintf(stderr, "필터 %s 파싱 실패: %s\n", filter_exp, pcap_geterr(handle));
-        pcap_close(handle);
-        return EXIT_FAILURE;
-    }
-    if (pcap_setfilter(handle, &fp) == -1) {
-        fprintf(stderr, "필터 %s 설치 실패: %s\n", filter_exp, pcap_geterr(handle));
-        pcap_close(handle);
-        return EXIT_FAILURE;
-    }
-
-    // 단계 3: 패킷 캡처
-    pcap_loop(handle, -1, got_packet, NULL);
-
-    pcap_close(handle); // 핸들 닫기
     return 0;
 }
